9ffa7a7fd5017eee874dec43a50dce7967325515
BingXing Wang <max@BingXings-MacBook-Pro.local>
2022-11-27 23:26:39 -0600

    fix ording to ordering

--- tweets/models.py
+++ tweets/models.py
@@ -10,7 +10,7 @@ class Tweet(models.Model):
 
     class Meta:
         index_together = (('user', 'created_at'),)
-        ording = ('user', '-created_at')
+        ordering = ('user', '-created_at')
 
     @property
     def hours_to_now(self):
